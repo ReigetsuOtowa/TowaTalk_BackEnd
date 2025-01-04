7e625211e9f40281940ef1087bafc0a0cb5f0ada
reigetsuotowa <reigetsuotowa@gmail.com>
2025-01-04 14:00:33 +0800

    1

---
+++ src/main/java/com/towatalk/towatalk_backend/TowaTalkBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.towatalk.towatalk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowaTalkBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TowaTalkBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/towatalk/towatalk_backend/TowaTalkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towatalk.towatalk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowaTalkBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
